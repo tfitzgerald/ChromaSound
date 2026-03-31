354c34d12eaa907f155f5fb7063547f8855999cf
tfitzgerald <tfitzgerald@rogers.com>
2026-03-31 16:27:30 -0400

    Increment versionCode from 2 to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.chromasound.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "2.3.3"
     }
     buildTypes {
